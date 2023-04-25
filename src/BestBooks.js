50076d1d416bcf1c166edf0abb5ba2be4066faea
Hayden James Cooper <haydencpr95@gmail.com>
2023-04-25 15:05:13 -0400

    First Card Complete Front end

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Button } from 'react-bootstrap';
 import axios from 'axios';
 import bookImg from './img/book.jpg';
+import BookFormModal from './BookFormModal';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
-      error: ''
+      error: '',
+      showBookFormModal: false // Track if the modal is visible or hidden
     }
   }
 
-  // componentDidMount() {
-  //   fetch('/books')
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       this.setState({ books: data });
-  //     })
-  //     .catch(error => {
-  //       console.error(error);
-  //     });
-  // }
-
-  async componentDidMount () {
+  async componentDidMount() {
     try {
       const config = {
         method: 'get',
@@ -44,24 +35,61 @@ class BestBooks extends React.Component {
     }
   }
 
+  // Method to add a new book to the list of books
+  addNewBook = async (newBookData) => {
+    try {
+      const config = {
+        method: 'post',
+        baseURL: process.env.REACT_APP_SERVER,
+        url: '/books',
+        data: newBookData
+      }
+      const response = await axios(config);
+      console.log('this is response', response);
+      // Add the new book to the list of books in state
+      this.setState((prevState) => ({
+        books: [...prevState.books, response.data],
+        error: ''
+      }));
+    } catch (error) {
+      console.error('Error with request', error);
+      this.setState({
+        error: `Status Code:${error.response.status}, ${error.response.data}`
+      })
+    }
+  }
+
+  // Method to toggle the visibility of the "Add Book" modal
+  toggleBookFormModal = () => {
+    this.setState((prevState) => ({
+      showBookFormModal: !prevState.showBookFormModal
+    }));
+  }
+
   render() {
     return (
       <div>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-        {this.state.books.length > 0 ?
-          <Carousel>
-            {this.state.books.map(book => (
-              <Carousel.Item key={book.id}>
+        <Button onClick={this.toggleBookFormModal}>Add Book</Button>
+        <Carousel>
+          {this.state.books.length > 0 ?
+            this.state.books.map(book => (
+              <Carousel.Item key={book._id}>
                 <img className="d-block w-100" src={bookImg} alt={book.title} />
                 <Carousel.Caption>
                   <h3>{book.title}</h3>
                   <p>{book.description}</p>
                 </Carousel.Caption>
               </Carousel.Item>
-            ))}
-          </Carousel>
-          : <h3>No books found.</h3>
-        }
+            ))
+            : <h3>No books found.</h3>
+          }
+        </Carousel>
+        <BookFormModal
+          show={this.state.showBookFormModal}
+          handleClose={this.toggleBookFormModal}
+          addNewBook={this.addNewBook}
+        />
       </div>
     )
   }
