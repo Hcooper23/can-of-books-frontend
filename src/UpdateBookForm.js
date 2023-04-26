e4bd571f3f5517804ab4e8fb03029d6a1e7f9ee9
Hayden James Cooper <haydencpr95@gmail.com>
2023-04-26 15:01:45 -0400

    Update form and module

--- src/UpdateBookForm.js
+++ src/UpdateBookForm.js
@@ -1,44 +1,50 @@
 import React, { Component } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 
-class BookFormModal extends Component {
-  state = {
-    title: '',
-    description: '',
-    status: '',
-    error: ''
+class UpdateBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      description: '',
+      status: '',
+      error: ''
+    };
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { title, description, status } = this.state;
+    const book = { title, description, status };
+
+    try {
+      const response = await fetch(`/books/${this.props.bookId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(book)
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update book');
+      }
+
+      const updatedBook = await response.json();
+
+      // Call the parent component's callback to update the book list
+      this.props.onUpdateBook(updatedBook);
+
+      // Close the modal
+      this.props.onHide();
+
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
   };
 
   handleInputChange = (event) => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
-  };
-
-  handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const { bookId, updateBook, handleClose } = this.props;
-    const { title, description, status } = this.state;
-    const updatedBook = { title, description, status };
-
-    try {
-      const response = await fetch(`/books/${bookId}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(updatedBook)
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to update book.');
-      }
-
-      const updatedBookData = await response.json();
-      updateBook(updatedBookData);
-      handleClose();
-    } catch (error) {
-      console.error(error);
-      this.setState({ error: 'Failed to update book. Please try again.' });
-    }
   };
 
   render() {
@@ -49,7 +55,7 @@ class BookFormModal extends Component {
       <Modal {...modalProps} size="lg" centered>
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            Edit Book
+            Update Book
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
@@ -59,8 +65,8 @@ class BookFormModal extends Component {
               <Form.Label>Title</Form.Label>
               <Form.Control
                 type="text"
-                placeholder="Enter title"
                 name="title"
+                placeholder="Enter title"
                 value={title}
                 onChange={this.handleInputChange}
               />
@@ -70,8 +76,8 @@ class BookFormModal extends Component {
               <Form.Control
                 as="textarea"
                 rows={3}
-                placeholder="Enter description"
                 name="description"
+                placeholder="Enter description"
                 value={description}
                 onChange={this.handleInputChange}
               />
@@ -90,7 +96,7 @@ class BookFormModal extends Component {
               </Form.Control>
             </Form.Group>
             <Button variant="primary" type="submit">
-              Submit
+              Update Book
             </Button>
           </Form>
         </Modal.Body>
@@ -99,4 +105,4 @@ class BookFormModal extends Component {
   }
 }
 
-export default BookFormModal;
+export default UpdateBook;
